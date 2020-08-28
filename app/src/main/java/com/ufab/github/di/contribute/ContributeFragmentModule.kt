4d91ad16228f6b79e37121d3e0391dd3bb70ca75
emburran <emburranlucifer@gmail.com>
2020-08-28 09:58:26 +0530

    viewmodel and linking

--- app/src/main/java/com/ufab/github/di/contribute/ContributeFragmentModule.kt
+++ app/src/main/java/com/ufab/github/di/contribute/ContributeFragmentModule.kt
@@ -1,8 +1,11 @@
 package com.ufab.github.di.contribute
 
 import com.ufab.github.di.FragmentScope
+import com.ufab.github.ui.contributorlist.ContributorFragment
+import com.ufab.github.ui.contributorlist.ContributorModule
 import com.ufab.github.ui.dashboard.homefragment.HomeFragment
 import com.ufab.github.ui.dashboard.homefragment.HomeFragmentModule
+import com.ufab.github.ui.reposotries.ReposotriesFragment
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -12,6 +15,14 @@ abstract class ContributeFragmentModule {
     @FragmentScope
     @ContributesAndroidInjector(modules = [HomeFragmentModule::class])
     abstract fun contributeHomeFragment(): HomeFragment
+
+    @FragmentScope
+    @ContributesAndroidInjector(modules = [ContributorModule::class])
+    abstract fun contributeorFragment(): ContributorFragment
+
+    @FragmentScope
+    @ContributesAndroidInjector(modules = [ContributorModule::class])
+    abstract fun repositoriesFragment(): ReposotriesFragment
 
 
 
